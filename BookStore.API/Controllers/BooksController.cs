599b2c131f50241f1a4f00d2411ea9955782bc17
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-04-03 00:12:04 +0600

    The completion of the backend part of the project

--- BookStore.API/Controllers/BooksController.cs
+++ BookStore.API/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using BookStore.API.Contracts;
 using BookStore.Application.Services;
+using BookStore.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.API.Controllers
@@ -19,9 +20,35 @@ namespace BookStore.API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<BooksResponse>>> GetBooks()
         {
-            var books =await _booksService.GetAllBooks();
-            var respons= books.Select(b=> new BooksResponse(b.Id,b.Title,b.Description,b.Price));
+            var books = await _booksService.GetAllBooks();
+            var respons = books.Select(b => new BooksResponse(b.Id, b.Title, b.Description, b.Price));
             return Ok(respons);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateBook([FromBody] BooksRequest request)
+        {
+            var (book, error) = Book.Create
+        (Guid.NewGuid(),
+        request.Title,
+        request.Description,
+        request.Price);
+            if (!string.IsNullOrEmpty(error))
+            {
+                return BadRequest(error);
+            }
+            var bookId = await _booksService.CreateBook(book);
+            return Ok(bookId);
+        }
+        [HttpPut("{id:guid}")]
+    public async Task<ActionResult<Guid>> UpdateBooks(Guid id, [FromBody] BooksRequest request)
+        {
+            var bookId= await _booksService.UpdateBook(id, request.Title, request.Description, request.Price);
+            return Ok(bookId);
+        }
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<Guid>>DeleteBook(Guid id)
+        {
+            return Ok(await _booksService.DeleteBook(id));
         }
     }
 }
