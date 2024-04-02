10327b0a37960c17f682b3eed9f39284f498b758
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-04-02 23:16:09 +0600

    Добавьте файлы проекта.

---
+++ BookStore.API/Program.cs
@@ -0,0 +1,37 @@
+using BookStore.Application.Services;
+using BookStore.DataAccess;
+using BookStore.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookStoreDBContext>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(BookStoreDBContext)));
+    } );
+builder.Services.AddScoped<IBooksService, BooksService>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BookStore.Core/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Models
+{
+    public class Book
+    {
+       public const int Max_title_length = 250;
+        public Book(Guid id, string title,string description, decimal price)
+        {
+            Id = id;    
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+
+        public Guid Id { get; }
+        public string Description { get;  }=string.Empty;
+        public string Title { get;  }=string.Empty;
+        public decimal Price { get; }
+        public static (Book Book,string Error) Create(Guid id, string title, string description, decimal price)
+        {
+            var error = string.Empty;
+            if (string.IsNullOrEmpty(title) || title.Length > Max_title_length)
+            {
+                error = "Title cane not be empty";
+            }
+            var book= new Book(id, title, description, price);
+            return(book,error);
+        }
+    }
+
+}
